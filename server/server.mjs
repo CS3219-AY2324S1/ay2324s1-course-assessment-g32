a303cfeba7d2e6c0ea4fcc47d4a28d8d9a73bf97
Venus Lim <50315976+venuslimm@users.noreply.github.com>
2023-09-04 23:28:26 +0800

    Update server.mjs comments

--- server/server.mjs
+++ server/server.mjs
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use("/record", records);
 
-// start Express server
+// start the Express server
 app.listen(PORT, () => {
 	console.log(`Server is running on port: ${PORT}`);
 });
