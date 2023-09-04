3015d294b7d322983941ace19aa612aa54a6f064
venuslimm <limjiayivenus@gmail.com>
2023-09-04 22:20:45 +0800

    Set up backbone for assignment

---
+++ server/db/conn.mjs
@@ -0,0 +1,17 @@
+import { MongoClient } from "mongodb";
+
+const connectionString = process.env.ATLAS_URI || "";
+
+const client = new MongoClient(connectionString);
+
+let conn;
+try {
+	conn = await client.connect();
+} catch (e) {
+	console.error(e);
+}
+
+// To update again to the name of our database
+let db = conn.db("sample_training");
+
+export default db;
