929e0e20480558b47340fd377e7fe993e4909049
JrmCkh <Hhelloogkart@gmail.com>
2023-09-07 11:35:05 +0800

    Add nodemon, mongoose and setup index.js on backend

---
+++ server/index.js
@@ -0,0 +1,14 @@
+// In src/index.js 
+const express = require("express"); 
+
+const app = express(); 
+const PORT = process.env.PORT || 3000; 
+
+// For testing purposes 
+app.get("/", (req, res) => { 
+    res.send("<h2>It's Working!</h2>"); 
+}); 
+
+app.listen(PORT, () => { 
+    console.log(`API is listening on port ${PORT}`); 
+});
