c28d92b36ebac0c652ecf997612559f66426a4c0
venuslimm <limjiayivenus@gmail.com>
2023-09-11 01:50:10 +0800

    Add MySQL to backend

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
-const cors = require('cors');
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const authRoutes = require('./server/routes/auth');
-require('dotenv').config();
+const cors = require("cors");
+const express = require("express");
+const mongoose = require("mongoose");
+const mysql = require("mysql2");
+const bodyParser = require("body-parser");
+const authRoutes = require("./server/routes/auth");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,20 +14,37 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Use the authRoutes for handling authentication-related routes
-app.use('/auth', authRoutes);
+app.use("/auth", authRoutes);
 
-mongoose.connect(MONGOCLIENT,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-);
+// MongoDB
+mongoose.connect(MONGOCLIENT, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
 
-const db = mongoose.connection;
-db.once('open', () => { console.log('Connected to the database'); });
-db.on('error', (error) => { console.error('Database connection error:', error); });
+const mongoDb = mongoose.connection;
+mongoDb.once("open", () => {
+	console.log("Connected to the MongoDB database");
+});
+mongoDb.on("error", (error) => {
+	console.error("MongoDB database connection error:", error);
+});
+
+// MySQL
+const mysqlDb = mysql.createConnection({
+	host: "localhost",
+	user: "root",
+	password: "password", // to update to your own password
+	database: "assignmentdb", // to match the database name
+});
+mysqlDb.connect((error) => {
+	if (error) {
+		console.error("MySQL database connection error:", error);
+	}
+	console.log("Connected to the MySQL database");
+});
 
 // start the Express (web) server
 app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
+	console.log(`Server is running on port: ${PORT}`);
 });
