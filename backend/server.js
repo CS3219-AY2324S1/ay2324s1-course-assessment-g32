f3734e94d561c6735f22b8972d4c074a17005663
lhy-hoyin <88828097+lhy-hoyin@users.noreply.github.com>
2023-09-11 16:42:17 +0800

    Update README and improve server.js

--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,18 @@ const bodyParser = require("body-parser");
 const authRoutes = require("./server/routes/auth");
 require("dotenv").config();
 
-const app = express();
+// Retrieve environment variables
 const PORT = process.env.PORT || 3000;
 const MONGOCLIENT = process.env.ATLAS_URI || "";
+const mysqlPassword = process.env.MY_SQL_PWD || "";
+const mysqlDbName = process.env.MY_SQL_DB_NAME || "";
 
+console.log("Starting server ...");
+
+const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-
-// Use the authRoutes for handling authentication-related routes
-app.use("/auth", authRoutes);
+app.use("/auth", authRoutes); // Use the authRoutes for handling authentication-related routes
 
 // MongoDB
 mongoose.connect(MONGOCLIENT, {
@@ -27,21 +30,21 @@ mongoDb.once("open", () => {
 	console.log("Connected to the MongoDB database");
 });
 mongoDb.on("error", (error) => {
-	console.error("MongoDB database connection error:", error);
+	console.error("MongoDB database connection error\n", error);
 });
 
 // MySQL
 const mysqlDb = mysql.createConnection({
 	host: "localhost",
 	user: "root",
-	password: "password", // to update to your own password
-	database: "assignmentdb", // to match the database name
+	password: mysqlPassword,
+	database: mysqlDbName,
 });
 mysqlDb.connect((error) => {
-	if (error) {
+	if (error)
 		console.error("MySQL database connection error:", error);
-	}
-	console.log("Connected to the MySQL database");
+	else
+		console.log("Connected to the MySQL database");
 });
 
 // start the Express (web) server
