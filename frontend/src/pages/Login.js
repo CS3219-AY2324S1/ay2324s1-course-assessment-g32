95079e99ca1429b4ee20cae3f97f42d4741067ca
JrmCkh <Hhelloogkart@gmail.com>
2023-09-11 21:39:48 +0800

    Connect frontend to backend api with axios

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react'
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 function Login() {
 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
-  const handleSignupClick = () => {
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSignupPageChange = () => {
     navigate("/signup");
+  };
+
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+
+    const userData = {
+      email: email,
+      password: password
+    };
+
+    axios.post("http://localhost:3000/auth/login", userData, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((response) => {
+      console.log(response);
+      navigate("/landing");
+    }).catch((error) => {
+      if (error.response.status === 400) {
+        console.error('Validation Error:', error.response.data.error);
+      } else {
+        console.error('Server Error:', error.message);
+      }
+    });
   };
 
   return (
@@ -16,7 +51,7 @@ function Login() {
           <h3 className="Auth-form-title">Sign In</h3>
           <div className="text-center">
             Not registered yet?{" "}
-            <span className="link-primary" onClick={handleSignupClick}>
+            <span className="link-primary" onClick={handleSignupPageChange}>
               Sign Up
             </span>
           </div>
@@ -26,6 +61,7 @@ function Login() {
               type="email"
               className="form-control mt-1"
               placeholder="Enter email"
+              onChange={handleEmailChange}
             />
           </div>
           <div className="form-group mt-3">
@@ -34,10 +70,11 @@ function Login() {
               type="password"
               className="form-control mt-1"
               placeholder="Enter password"
+              onChange={handlePasswordChange}
             />
           </div>
           <div className="d-grid gap-2 mt-3">
-            <button type="submit" className="btn btn-primary">
+            <button type="submit" className="btn btn-primary" onClick={handleLoginSubmit}>
               Submit
             </button>
           </div>
