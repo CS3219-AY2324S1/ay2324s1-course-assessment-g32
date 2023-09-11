3efcd1c1cec07f1549a902ebb2b4ad0265878539
JrmCkh <Hhelloogkart@gmail.com>
2023-09-11 18:41:34 +0800

    Add navigate between Login and Signup page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
-import Landing from './pages/Landing/Landing';
+import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import Landing from './pages/Landing/Landing';
 import './App.css';
 import './css/Auth.css';
 
 function App() {
   return (
-    <Signup />
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/landing" element={<Landing />} />
+      <Route path="/" element={<Login />} /> {/* Display Login component by default */}
+    </Routes>
   );
 }
 
