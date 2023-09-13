4ce7feba4f2c525b934060b4b3f85d13cf6aaecf
JrmCkh <Hhelloogkart@gmail.com>
2023-09-14 00:35:42 +0800

    Change QuestionList UI to use bootstrap and dataTables plugin instead of materials-ui

--- frontend/src/components/QuestionList/QuestionList.js
+++ frontend/src/components/QuestionList/QuestionList.js
@@ -1,102 +1,83 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { useNavigate } from 'react-router-dom';
-import { DataGrid } from '@mui/x-data-grid'
-import { Box } from '@mui/material'
-import Button from '@mui/material/Button';
-import AddIcon from '@mui/icons-material/Add';
-import Stack from '@mui/material/Stack';
+import $ from 'jquery';
+import 'datatables.net';
+import 'datatables.net-bs5/css/dataTables.bootstrap5.min.css';
 import { getQuestions } from '../../api/QuestionApi.js';
 import { showServerErrorToast } from '../../utils/toast.js';
 import './QuestionList.css';
 
-const columns = [
-  { field: 'no', headerName: 'No.', width: 100, headerClassName: 'theme--header' },
-  { field: 'title', headerName: 'Title', width: 800, headerClassName: 'theme--header' },
-  { field: 'complexity', headerName: 'Complexity', width: 200, headerClassName: 'theme--header' }
-]
-
 const QuestionList = () => {
 
   const [tableData, setTableData] = useState([])
+  const tableRef = useRef(null);
+  const dataTableRef = useRef(null);
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchDataAndInitializeTable = async () => {
       try {
         const questions = await getQuestions();
+        console.log(questions);
         setTableData(questions);
       } catch (error) {
         showServerErrorToast(error);
       };
     };
 
-    fetchData();
+    fetchDataAndInitializeTable();
   }, []);
 
+  useEffect(() => {
+    // Initialize DataTables after the data has been set and the table is rendered
+    if (tableRef.current && tableData.length > 0) {
+      // Destroy the existing DataTable instance if it exists
+      if (dataTableRef.current) {
+        dataTableRef.current.destroy();
+      }
+
+      // Initialize DataTables
+      dataTableRef.current = $(tableRef.current).DataTable();
+    }
+  }, [tableData]); // Initialize whenever tableData changes
+
   const navigate = useNavigate();
-  const handleRowClick = (params) => {
-    navigate('/question/' + params.row._id);
+  const handleRowClick = (id) => {
+    navigate('/question/' + id);
   };
 
   const handleNewQuestionClick = () => {
     navigate('/new');
   };
 
+  const questionList = tableData.map((question, index) => (
+    <tr key={question._id} onClick={() => handleRowClick(question._id)}>
+      <th scope="row">{index + 1}</th>
+      <td>{question.title}</td>
+      <td>{question.complexity}</td>
+    </tr>
+  ));
+
   return (
-    <Box bgcolor="#2d2d2d" sx={{ height: '80vh', width: '80%', borderRadius: '25px' }}>
-      <DataGrid
-        rows={
-          tableData?.map((row, index) => {
-            return {
-              no: index + 1,
-              ...row
-            }
-          })
-        }
-        columns={columns}
-        getRowId={(row) => row._id}
-        onRowClick={handleRowClick}
-        pageSize={12}
-        hideFooterPagination
-        hideFooterSelectedRowCount
-        hideFooter
-        rowHeight={60}
-        components={{
-          NoRowsOverlay: () => (
-            <Stack height="100%" alignItems="center" justifyContent="center">
-              <h2>
-                No Questions Found!
-              </h2>
-            </Stack>
-          ),
-          NoResultsOverlay: () => (
-            <Stack height="100%" alignItems="center" justifyContent="center">
-              <h2>
-                Search Found No Questions!
-              </h2>
-            </Stack>
-          )
-        }}
-        sx={{
-          boxShadow: 2,
-          border: 2,
-          p: 2,
-          borderRadius: '25px',
-          '.MuiDataGrid-columnHeader, & .MuiDataGrid-cell': {
-            color: "white",
-            fontWeight: 600,
-          },
-          '& .theme--header': {
-            backgroundColor: 'rgba(white, 0.55)',
-          },
-        }}
-      />
-
-      <Button style={{ maxWidth: '110px', minWidth: '110px', float: 'right' }} variant="contained"
-        sx={{ m: 2 }} onClick={handleNewQuestionClick} color="success" startIcon={<AddIcon />}>
-        Add
-      </Button>
-    </Box>
-
+    <div class="container">
+      <h1>Question List</h1>
+      <table ref={tableRef} class="table table-hover table-striped">
+        <thead class="table-dark">
+          <tr>
+            <th scope="col" width="100">No.</th>
+            <th scope="col" width="800">Title</th>
+            <th scope="col" width="200">Complexity</th>
+          </tr>
+        </thead>
+        <tbody class="table-group-divider">
+          {questionList}
+        </tbody>
+      </table>
+      <div className="text-md-end">
+        <button type="button" className="btn btn-success" onClick={handleNewQuestionClick}>
+          Add
+        </button>
+      </div>
+    </div>
   )
 };
 
