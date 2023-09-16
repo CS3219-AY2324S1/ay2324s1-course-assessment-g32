9145243d343f648f3790605b76fe6489dda8aa3a
venuslimm <limjiayivenus@gmail.com>
2023-09-17 00:37:50 +0800

    Allow user to access all routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
@@ -7,21 +7,17 @@ import UserManagement from './pages/UserManagement/UserManagement';
 import UserProfile from './pages/UserProfile/UserProfile';
 import './App.css';
 import './css/Auth.css';
-import { useRef } from 'react';
 
 function App() {
-  const user = useRef(null);
-  user.current = JSON.parse(localStorage.getItem('user'));
-
   return (
     <div>
       <Routes>
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
-        {/* TODO: Create ProtectedRoute to handle the routes below */}
-        <Route path='/landing/*' element={user.current ? <Landing /> : <Navigate to='/login' />} /> {/* TODO: create an unauthorised action page */}
-        <Route path='/user-profile/*' element={user.current ? <UserProfile /> : <Navigate to='/login' />} /> {/* TODO: create an unauthorised action page */}
-        <Route path='/user-management/*' element={user.current && user.current.isAdmin ? <UserManagement /> : <Navigate to='/login' />} /> {/* TODO: create an unauthorised action page */}
+        {/* TODO: Create ProtectedRoute to handle the routes for authenticated users */}
+        <Route path='/landing/*' element={<Landing />} /> {/* TODO: create an unauthorised action page */}
+        <Route path='/user-profile/*' element={<UserProfile />} /> {/* TODO: create an unauthorised action page */}
+        <Route path='/user-management/*' element={<UserManagement />} /> {/* TODO: create an unauthorised action page */}
         <Route path='/' element={<Login />} /> {/* Display Login component by default */}
       </Routes>
       <ToastContainer />
