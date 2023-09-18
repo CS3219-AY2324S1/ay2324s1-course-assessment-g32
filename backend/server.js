e2eadac23e245071bacdcf09d8a1ff57a5b5497b
JrmCkh <Hhelloogkart@gmail.com>
2023-09-18 20:04:10 +0800

    Update server.js to include both auth and question routes, and MongoDB and SQL connection

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
+const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const authRoutes = require('./server/routes/auth');
 const questionRoutes = require('./server/routes/question');
@@ -8,29 +9,41 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const MONGOCLIENT = process.env.ATLAS_URI || "";
-
-app.use(cors());
-app.use(bodyParser.json());
-
-// Use the authRoutes for handling authentication-related routes
-app.use('/auth', authRoutes);
-
-// Use the questionRoutes for handling question-related routes
-app.use('/question', questionRoutes);
-
-mongoose.connect(MONGOCLIENT,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-);
-
-const db = mongoose.connection;
-db.once('open', () => { console.log('Connected to the database'); });
-db.on('error', (error) => { console.error('Database connection error:', error); });
+const MONGOCLIENT = process.env.ATLAS_URI || '';
 
 // start the Express (web) server
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/auth', authRoutes);
+app.use('/question', questionRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
+});
+
+// MongoDB
+mongoose.connect(MONGOCLIENT, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const mongoDb = mongoose.connection;
+mongoDb.once('open', () => {
+  console.log('Connected to the MongoDB database');
+});
+mongoDb.on('error', (error) => {
+  console.error('MongoDB database connection error:', error);
+});
+
+// MySQL
+const mysqlDb = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password', // to update to your own password
+  database: 'assignmentdb', // to match the database name
+});
+mysqlDb.connect((error) => {
+  if (error) {
+    console.error('MySQL database connection error:', error);
+  }
+  console.log('Connected to the MySQL database');
 });
