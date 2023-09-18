bf105362ccb9fe5f9efafab30ad17a89b264619c
JrmCkh <Hhelloogkart@gmail.com>
2023-09-18 20:37:00 +0800

    Update user controller

--- backend/server/controllers/userController.js
+++ backend/server/controllers/userController.js
@@ -49,10 +49,21 @@ const deleteUser = async (req, res) => {
   }
 };
 
+const changePassword = async (req, res) => {
+  try {
+    const { id, currentPassword, newPassword, confirmPassword } = req.body;
+    await userService.changeUserPassword(id, currentPassword, newPassword, confirmPassword);
+    res.json({ message: 'SUCCESS: Password changed' });
+  } catch (err) {
+    res.status(err?.status || 400).json({ error: err?.message || err });
+  }
+};
+
 module.exports = {
   createUser,
   getAllUserInfo,
   getUserInfo,
   updateUser,
   deleteUser,
+  changePassword
 };
