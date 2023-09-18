527cd245ba7a816c7ccf67aa9ab556d2bb5bf496
JrmCkh <Hhelloogkart@gmail.com>
2023-09-18 21:49:23 +0800

    Update code quality of controllers

--- backend/server/controllers/authController.js
+++ backend/server/controllers/authController.js
@@ -1,19 +1,5 @@
 const userService = require('../services/userService');
 
-// Define a controller function for handling signup requests
-const signup = async (req, res) => {
-  try {
-    const { email, password, confirmPassword } = req.body;
-    await userService.createUser(email, password, confirmPassword);
-    res.json({ message: 'User registered successfully' });
-  } catch (err) {
-    res
-      .status(err?.status || 500)
-      .json({ error: err?.message || err });
-  }
-};
-
-// TODO: test code
 // Define a controller function for handling login requests
 const login = async (req, res) => {
   try {
@@ -29,7 +15,20 @@ const login = async (req, res) => {
   }
 };
 
+// Define a controller function for handling signup requests
+const signup = async (req, res) => {
+  try {
+    const { email, password, confirmPassword } = req.body;
+    await userService.createUser(email, password, confirmPassword);
+    res.json({ message: 'User registered successfully' });
+  } catch (err) {
+    res
+      .status(err?.status || 500)
+      .json({ error: err?.message || err });
+  }
+};
+
 module.exports = {
   login,
-  signup, 
+  signup
 };
