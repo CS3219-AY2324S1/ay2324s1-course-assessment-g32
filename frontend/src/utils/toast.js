91e1a81d4922911635942c1169fa550e9ac74f2f
JrmCkh <Hhelloogkart@gmail.com>
2023-09-19 22:20:25 +0800

    Remove extra ,

--- frontend/src/utils/toast.js
+++ frontend/src/utils/toast.js
@@ -3,19 +3,19 @@ import 'react-toastify/dist/ReactToastify.css';
 
 export const showSuccessToast = (message) => {
   toast.success(message, {
-    position: toast.POSITION.BOTTOM_RIGHT,
+    position: toast.POSITION.BOTTOM_RIGHT
   });
 };
 
 export const showFailureToast = (message) => {
   toast.error(message, {
-    position: toast.POSITION.BOTTOM_RIGHT,
+    position: toast.POSITION.BOTTOM_RIGHT
   });
 }
 
 export const showValidationErrorToast = (error) => {
   toast.error('Validation Error: ' + error.response.data.error, {
-    position: toast.POSITION.BOTTOM_RIGHT,
+    position: toast.POSITION.BOTTOM_RIGHT
   });
 };
 
@@ -28,12 +28,12 @@ export const showServerErrorToast = (error) => {
 
 export const showDuplicateQuestionErrorToast = (error) => {
   toast.error('Duplicate Question Error: ' + error.response.data.error, {
-    position: toast.POSITION.BOTTOM_RIGHT,
+    position: toast.POSITION.BOTTOM_RIGHT
   });
 };
 
 export const showQuestionNotFoundErrorToast = (error) => {
   toast.error('Question Not Found Error: ' + error.response.data.error, {
-    position: toast.POSITION.BOTTOM_RIGHT,
+    position: toast.POSITION.BOTTOM_RIGHT
   });
 };
