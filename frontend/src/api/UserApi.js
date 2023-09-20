cf8a79e096ec2baaaa385e054ab3cbfdf0a28911
anthonynmh <anthonyneomh@gmail.com>
2023-09-21 02:59:05 +0800

    Abstract content type header

--- frontend/src/api/UserApi.js
+++ frontend/src/api/UserApi.js
@@ -1,76 +1,46 @@
 import axios from 'axios';
 
+const config = {
+  headers: {
+    'Content-Type': 'application/json'
+  }
+};
+
 export const signup = async (userData) => {
-  return axios.post('http://localhost:3000/auth/signup', userData, {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+  return axios.post('http://localhost:3000/auth/signup', userData, config);
 };
 
 export const login = async (userData) => {
-  return axios.post('http://localhost:3000/auth/login', userData, {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+  return axios.post('http://localhost:3000/auth/login', userData, config);
 };
 
 export const getAllUsers = async () => {
-  const res = await axios.get('http://localhost:3000/user/readAll', {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+  const res = await axios.get('http://localhost:3000/user/readAll', config);
   return res.data.info;
 };
 
 export const getUser = async (id) => {
-  const res = await axios.post(
-    'http://localhost:3000/user/read',
-    { id },
-    {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    }
-  );
+  const res = await axios.post('http://localhost:3000/user/read', { id }, config);
   return res.data.info;
 };
 
 export const updateUsername = async (id, newUsername) => {
-  const res = await axios.post(
-    'http://localhost:3000/user/update',
-    { id: id, username: newUsername },
-    {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    }
-  );
+  const res = await axios.post('http://localhost:3000/user/update', { id: id, username: newUsername }, config);
   return res;
 };
 
 export const updatePassword = (id, currentPassword, newPassword, confirmPassword) => {
   return axios.post(
     'http://localhost:3000/user/change-password',
-    { id: id, currentPassword: currentPassword, newPassword: newPassword, confirmPassword: confirmPassword },
-    {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    }
+    { id: id, 
+      currentPassword: currentPassword, 
+      newPassword: newPassword, 
+      confirmPassword: confirmPassword 
+    },
+    config
   );
 };
 
 export const deleteUser = (id) => {
-  return axios.post(
-    'http://localhost:3000/user/delete',
-    { id },
-    {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    }
-  );
+  return axios.post('http://localhost:3000/user/delete', { id }, config);
 };
