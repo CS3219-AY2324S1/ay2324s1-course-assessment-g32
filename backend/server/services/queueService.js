edd00a6e9ca82ef64936afe3fe8c2e7fe8c467d6
Caden <63772723+cadencjk@users.noreply.github.com>
2023-09-24 02:27:01 +0800

    Implement response message in queueing service

--- backend/server/services/queueService.js
+++ backend/server/services/queueService.js
@@ -1,33 +1,52 @@
 const amqp = require('amqplib');
 
-rabbitMQ = {
-  url: 'amqp://localhost',
-  exchange: 'findOpponent',
+const generateUuid = () => {
+  return "id" + Math.random().toString(16).slice(2);
 };
 
-class QueueService {
-  channel;
+const joinQueue = async (complexityType, id) => {
+  const responseQueue = complexityType + 'ResponseQueue';
 
-  async createChannel () {
-    const connection = await amqp.connect(rabbitMQ.url);
-    this.channel = await connection.createChannel();
-  };
+  const connection = await amqp.connect('amqp://localhost');
+  const channel = await connection.createChannel();
 
-  async joinQueue (complexityType, id) {
-    if (!this.channel) {
-      await this.createChannel();
-    }
+  await channel.assertQueue(complexityType, { durable: false });
+  await channel.assertQueue(responseQueue, { durable: false });
 
-    await this.channel.assertExchange(rabbitMQ.exchange, 'direct');
+  // Create a unique Id for the request
+  const correlationId = generateUuid();
 
-    await this.channel.publish(rabbitMQ.exchange, complexityType,
-      Buffer.from(JSON.stringify({
-        complexityType: complexityType,
-        id: id,
-        dateTime: new Date(),
-      }))
-    );
-  };
+  // Send the request to the queue
+  channel.sendToQueue(complexityType, Buffer.from(JSON.stringify({
+    complexityType: complexityType,
+    id: id,
+    replyTo: responseQueue,
+    correlationId: correlationId,
+  })));
+
+  // Variable to check if the connection is closed
+  var isClosed = false;
+
+  return new Promise((resolve, reject) => {
+    // Wait for the response with matching correlation ID
+    channel.consume(responseQueue, (response) => {
+      if (response.properties.correlationId === correlationId) {
+
+        const responseBody = JSON.parse(response.content.toString());
+        resolve(responseBody);
+
+        channel.ack(response);
+
+        // Close the connection
+        isClosed = true;
+        channel.close();
+        connection.close();
+
+      }
+    }, { noAck: false });
+  });
 };
 
-module.exports = QueueService;
+module.exports = {
+  joinQueue
+};
