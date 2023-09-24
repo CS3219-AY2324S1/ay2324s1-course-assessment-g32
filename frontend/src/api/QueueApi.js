14d250254610173641cbb975ae56785169d51ff3
Caden <63772723+cadencjk@users.noreply.github.com>
2023-09-24 16:21:07 +0800

    Connect API from frontend to backend for matching service

---
+++ frontend/src/api/QueueApi.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+const env = require("../loadEnvironment");
+
+const rootUrl = env.SERVER_URL + '/queue';
+
+const config = {
+  headers: {
+    'Content-Type': 'application/json'
+  }
+};
+
+export const joinQueue = async (user, complexityType) => {
+  try {
+    const data = {
+      id: user.id,
+      complexityType: complexityType
+    };
+    return await axios.post(rootUrl + "/join", data, config);
+  } catch (err) {
+    if (err.code === 'ERR_NETWORK') {
+      throw Object.assign(new Error(err.code), { response: { status: 408 }, message: 'Network Error' });
+    }
+    throw err;
+  }
+};
+
+export const exitQueue = async (user, complexityType) => {
+  try {
+    const data = {
+      id: user.id,
+      complexityType: complexityType
+    };
+    return await axios.post(rootUrl + "/exit", data, config);
+  } catch (err) {
+    if (err.code === 'ERR_NETWORK') {
+      throw Object.assign(new Error(err.code), { response: { status: 408 }, message: 'Network Error' });
+    }
+    throw err;
+  }
+};
