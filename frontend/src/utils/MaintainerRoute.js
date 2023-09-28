c29ea140877752b3b46f775c4756f88477d6bf85
anthonynmh <anthonyneomh@gmail.com>
2023-09-28 16:41:49 +0800

    resolve conflict

--- frontend/src/utils/MaintainerRoute.js
+++ frontend/src/utils/MaintainerRoute.js
@@ -3,30 +3,27 @@ import { Navigate, Outlet } from 'react-router-dom';
 import Cookies from 'js-cookie';
 import decode from 'jwt-decode';
 
-const MaintainerRoute = () => {
-  const isMaintainer = () => {
-    try {
-      const token = Cookies.get('jwt');
-      // console.log("token: " + token)
+export const isMaintainer = () => {
+  try {
+    const token = Cookies.get('jwt');
 
-      if (!token) {
-        // console.log("no token found")
-        return false;
-      } else {
-        const decodedToken = decode(token, 'password');
-        if (decodedToken) {
-          return decodedToken.isMaintainer !== 1 ? false : true;
-        } else {
-          // console.log("unable to decode token")
-          return false;
-        }
-      }
-    } catch (err) {
-      console.error('Cookie not found');
+    if (!token) {
       return false;
+    } else {
+      const decodedToken = decode(token, 'password');
+      if (decodedToken) {
+        return decodedToken.isMaintainer !== 1 ? false : true;
+      } else {
+        return false;
+      }
     }
-  };
+  } catch (err) {
+    console.error('Cookie not found');
+    return false;
+  }
+};
 
+const MaintainerRoute = () => {
   // If authorized, return an outlet that will render child elements
   // If not, return element that will navigate to login page
   return isMaintainer() ? <Outlet /> : <Navigate to='/unauthorised' />;
