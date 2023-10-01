fd7f99645bbc209665ea6f16e7755347db551e80
lhy-hoyin <88828097+lhy-hoyin@users.noreply.github.com>
2023-10-01 15:27:16 +0800

    Post A-2: explictly state status code for success

--- backend/server/controllers/userController.js
+++ backend/server/controllers/userController.js
@@ -4,7 +4,7 @@ const createUser = async (req, res) => {
   try {
     const { email, password, confirmPassword } = req.body;
     await userService.createUser(email, password, confirmPassword);
-    res.json({ message: 'SUCCESS: User created' });
+    res.status(200).json({ message: 'SUCCESS: User created' });
   } catch (err) {
     res.status(err?.status || 500).json({ error: err?.message || err });
   }
@@ -13,7 +13,7 @@ const createUser = async (req, res) => {
 const getAllUserInfo = async (req, res) => {
   try {
     const info = await userService.getAllUserInfo();
-    res.json({ message: 'SUCCESS', info });
+    res.status(200).json({ info });
   } catch (err) {
     res.status(err?.status || 400).json({ error: err?.message || err });
   }
@@ -23,7 +23,7 @@ const getUserInfo = async (req, res) => {
   try {
     const { id, email } = req.body;
     const info = await userService.getUserInfo(id, email);
-    res.json({ message: 'SUCCESS', info });
+    res.status(200).json({ info });
   } catch (err) {
     res.status(err?.status || 400).json({ error: err?.message || err });
   }
@@ -33,7 +33,7 @@ const updateUser = async (req, res) => {
   try {
     const { id, username, password } = req.body;
     await userService.updateUser(id, username, password);
-    res.json({ message: 'SUCCESS: User info updated' });
+    res.status(200).json({ message: 'SUCCESS: User info updated' });
   } catch (err) {
     res.status(err?.status || 400).json({ error: err?.message || err });
   }
@@ -43,7 +43,7 @@ const deleteUser = async (req, res) => {
   try {
     const { id } = req.body;
     await userService.deleteUser(id);
-    res.json({ message: 'SUCCESS: User deleted' });
+    res.status(200).json({ message: 'SUCCESS: User deleted' });
   } catch (err) {
     res.status(err?.status || 400).json({ error: err?.message || err });
   }
@@ -53,7 +53,7 @@ const changePassword = async (req, res) => {
   try {
     const { id, currentPassword, newPassword, confirmPassword } = req.body;
     await userService.changeUserPassword(id, currentPassword, newPassword, confirmPassword);
-    res.json({ message: 'SUCCESS: Password changed' });
+    res.status(200).json({ message: 'SUCCESS: Password changed' });
   } catch (err) {
     res.status(err?.status || 400).json({ error: err?.message || err });
   }
