b8b08215027fecc94620be951fb9b9d3bebf95c3
lhy-hoyin <88828097+lhy-hoyin@users.noreply.github.com>
2023-10-05 15:55:46 +0800

    Allow edit user to change password from UPM

--- UsersManagement/src/components/User/EditUser.js
+++ UsersManagement/src/components/User/EditUser.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
-import { updateUsername } from 'api/UserApi.js';
+import { updateUsername, updatePassword } from 'api/UserApi.js';
 import { showValidationErrorToast, showServerErrorToast, showSuccessToast } from 'utils/toast.js';
 
 const EditUser = ({ user = null }) => {
   const [id, setId] = useState(null);
   const [newUsername, setUsername] = useState('');
+  const [currentPassword, setCurrentPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
   const [isLoading, setIsLoading] = useState(true);
 
   const location = useLocation();
@@ -39,11 +42,27 @@ const EditUser = ({ user = null }) => {
           showServerErrorToast(error);
         }
       });
+  
+    if (currentPassword && newPassword && confirmPassword) {
+      updatePassword(id, currentPassword, newPassword, confirmPassword)
+        .then(() => {
+          navigate(-1);
+          showSuccessToast('Username updated successfully!');
+        })
+        .catch((error) => {
+          if (error.response.status === 400) {
+            showValidationErrorToast(error);
+          } else {
+            showServerErrorToast(error);
+          }
+        });
+    }
   };
 
-  const handleUsernameChange = (event) => {
-    setUsername(event.target.value);
-  };
+  const handleUsernameChange = (event) => { setUsername(event.target.value) };
+  const handleCurPasswordChange = (event) => { setCurrentPassword(event.target.value) };
+  const handleNewPasswordChange = (event) => { setNewPassword(event.target.value) };
+  const handleCfmPasswordChange = (event) => { setConfirmPassword(event.target.value) };
 
   return isLoading ? (
     <div className='spinner-border text-primary' role='status'>
@@ -69,9 +88,24 @@ const EditUser = ({ user = null }) => {
       </div>
 
       <form className='edit-username needs-validation' onSubmit={handleUpdateClick} noValidate>
+        Update Username:
         <div className='form-floating mb-3'>
           <input type='text' className='form-control' id='editUsername' placeholder='username' value={newUsername} onChange={handleUsernameChange} required />
           <label htmlFor='editUsername'>Username</label>
+        </div>
+        Update Password: <br/>
+        Note: No change made if any fields below are empty
+        <div className='form-floating mb-3'>
+          <input type='text' className='form-control' id='current-password'  value={currentPassword} onChange={handleCurPasswordChange} />
+          <label htmlFor='current-password'>Current Password</label>
+        </div>
+        <div className='form-floating mb-3'>
+          <input type='text' className='form-control' id='new-password'  value={newPassword} onChange={handleNewPasswordChange} />
+          <label htmlFor='new-password'>New Password</label>
+        </div>
+        <div className='form-floating mb-3'>
+          <input type='text' className='form-control' id='confirm-password' value={confirmPassword} onChange={handleCfmPasswordChange} />
+          <label htmlFor='confirm-password'>Confirm New Password</label>
         </div>
         <div className='d-flex justify-content-end'>
           <button type='submit' className='btn btn-success'>
