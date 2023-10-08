bc38ec15922112fbac4e32e73fb8d91716121e86
venuslimm <limjiayivenus@gmail.com>
2023-10-08 19:11:19 +0800

    Update env variables

--- frontend/src/loadEnvironment.js
+++ frontend/src/loadEnvironment.js
@@ -1,6 +1,6 @@
 const SERVER_HOST = process.env.REACT_APP_HOST || 'http://localhost';
-const SERVER_PORT = process.env.REACT_APP_SERVER_PORT || 5000;
-const AUTH_PORT = process.env.REACT_APP_AUTH_PORT || 5001;
+const SERVER_PORT = process.env.SERVER_PORT || 3001;
+const AUTH_PORT = process.env.AUTH_PORT || 5001;
 const SERVER_URL = SERVER_HOST + ':' + SERVER_PORT;
 const AUTH_URL = SERVER_HOST + ':' + AUTH_PORT;
 
