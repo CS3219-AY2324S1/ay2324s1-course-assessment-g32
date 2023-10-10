506a69644a5c3f10675189dd088f5ade20ba7de9
venuslimm <limjiayivenus@gmail.com>
2023-10-10 14:09:20 +0800

    Update env variable names

--- frontend/src/loadEnvironment.js
+++ frontend/src/loadEnvironment.js
@@ -1,5 +1,5 @@
 const SERVER_HOST = process.env.REACT_APP_HOST || 'http://localhost';
-const SERVER_PORT = process.env.SERVER_PORT || 3001;
+const SERVER_PORT = process.env.REACT_APP_SERVER_PORT || 3001;
 const SERVER_URL = SERVER_HOST + ':' + SERVER_PORT;
 
 module.exports = {
