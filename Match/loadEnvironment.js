119250d2868278d6509328b2892d9823549624af
Caden <63772723+cadencjk@users.noreply.github.com>
2023-10-14 12:50:35 +0800

    Extract out consumer service

--- Match/loadEnvironment.js
+++ Match/loadEnvironment.js
@@ -6,10 +6,12 @@ const AUTH_PORT = process.env.AUTH_PORT || 5001;
 
 const AUTH_URL = REACT_APP_HOST + ':' + AUTH_PORT;
 
+const RABBITMQ_URL = process.env.RABBITMQ_URL || 'amqp://localhost';
 
 const MATCH_PORT = process.env.MATCH_PORT || 7001;
 
 module.exports = {
   MATCH_PORT,
   AUTH_URL,
+  RABBITMQ_URL,
 };
