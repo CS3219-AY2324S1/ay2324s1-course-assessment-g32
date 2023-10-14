2d5aa183d37081c0f28777347afae3d32350327d
anthonynmh <anthonyneomh@gmail.com>
2023-10-15 06:45:00 +0800

    Include Question url in frontend/loadenv js

--- frontend/src/loadEnvironment.js
+++ frontend/src/loadEnvironment.js
@@ -1,10 +1,13 @@
 const SERVER_HOST = process.env.REACT_APP_HOST || 'http://localhost';
 const SERVER_PORT = process.env.REACT_APP_SERVER_PORT || 3001;
 const AUTH_PORT = process.env.REACT_APP_AUTH_PORT || 5001;
+const QUESTION_PORT = process.env.REACT_APP_QUESTION_PORT || 6001;
+const QUESTION_URL = SERVER_HOST + ':' + QUESTION_PORT;
 const SERVER_URL = SERVER_HOST + ':' + SERVER_PORT;
 const AUTH_URL = SERVER_HOST + ':' + AUTH_PORT;
 
 module.exports = {
   SERVER_URL,
   AUTH_URL,
+  QUESTION_URL,
 };
