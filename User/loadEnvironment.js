7af2a2a4c38ce30f0e5016d56f11798fe61440c5
lhy-hoyin <88828097+lhy-hoyin@users.noreply.github.com>
2023-10-15 16:26:45 +0800

    Update .ev format + fix package.json files

--- User/loadEnvironment.js
+++ User/loadEnvironment.js
@@ -7,12 +7,12 @@ const USER_PORT = process.env.USER_PORT || 4001;
 const AUTH_PORT = process.env.AUTH_PORT || 5001;
 const AUTH_URL = REACT_APP_HOST + ':' + AUTH_PORT;
 
-const mysqlDbName = process.env.MY_SQL_DB_NAME || '';
+const mysqlDbName = process.env.MYSQL_DB_NAME || '';
 const mysqlCreds = {
-  port: process.env.MY_SQL_PORT || 3306,
-  host: process.env.MY_SQL_HOST || 'localhost',
-  user: process.env.MY_SQL_USER || 'root',
-  password: process.env.MY_SQL_PWD || '',
+  port: process.env.MYSQL_PORT || 3306,
+  host: process.env.MYSQL_HOST || 'localhost',
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_ROOT_PASSWORD || '',
 };
 
 module.exports = {
