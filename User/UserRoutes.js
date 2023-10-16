0538e7868e9311f8bc460ae369286aa6f1756b20
JrmCkh <Hhelloogkart@gmail.com>
2023-10-16 14:34:08 +0800

    Update code quality on User routes

--- User/UserRoutes.js
+++ User/UserRoutes.js
@@ -8,21 +8,13 @@ const middleware = require('./helpers/middleware.js');
 router.post('/login', userController.login, middleware.getToken);
 
 // Only maintainer can access these routes
-router.get(
-  '/readAll',
-  middleware.checkTokenMaintainer,
-  userController.getAllUserInfo
-);
+router.get('/readAll', middleware.checkTokenMaintainer, userController.getAllUserInfo);
 
 // User and maintainer can access the remaining routes
 router.post('/read', middleware.checkToken, userController.getUserInfo);
 router.post('/update', middleware.checkToken, userController.updateUser);
 router.post('/delete', middleware.checkToken, userController.deleteUser);
-router.post(
-  '/change-password',
-  middleware.checkToken,
-  userController.changePassword
-);
+router.post('/change-password', middleware.checkToken, userController.changePassword);
 
 // No middleware function
 router.post('/signup', userController.signup);
