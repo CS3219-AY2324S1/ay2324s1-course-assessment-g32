7240f75fc3df7d913d63e7042eabc44f16c51c6f
Caden <63772723+cadencjk@users.noreply.github.com>
2023-10-27 18:31:47 +0800

    Change landing page to welcome user with display name

--- frontend/src/pages/Landing/Landing.js
+++ frontend/src/pages/Landing/Landing.js
@@ -1,23 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import SubmissionHeatMap from '../../components/Statistics/SubmissionHeatMap';
 import SubmissionPieChart from '../../components/Statistics/SubmissionPieChart';
 import MatchingModal from '../../components/MatchMaking/MatchingModal';
 import Header from '../../components/Header';
+import { getUserId, getCookie } from '../../utils/helpers';
+import { getUser } from '../../api/UserApi';
+import { errorHandler } from '../../utils/errors';
 import './Landing.css';
 
 function Landing() {
 
+  const [user, setUser] = useState('');
   const [isMatchingModalOpen, setMatchingModalOpen] = useState(false);
 
   const handleToggleModal = () => {
     setMatchingModalOpen(!isMatchingModalOpen);
   };
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const userId = await getUserId();
+        const response = await getUser(userId, getCookie());
+        setUser(response.username);
+      } catch (error) {
+        errorHandler(error);
+      }
+    };
+
+    fetchData();
+  }, [setUser]);
+
   return (
     <div className='landing'>
       <Header />
-      <h1 className='title m-5'>Welcome, User</h1>
+      <h1 className='title m-5'>Welcome, {user}</h1>
       <SubmissionPieChart />
       <SubmissionHeatMap />
       <div className='d-flex align-items-center justify-content-center'>
