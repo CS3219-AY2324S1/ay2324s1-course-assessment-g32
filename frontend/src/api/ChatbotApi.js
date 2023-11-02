c55e96658536860127e5f94261373959bc51c39d
anthonynmh <anthonyneomh@gmail.com>
2023-11-03 06:43:00 +0800

    Remove req queue in api

--- frontend/src/api/ChatbotApi.js
+++ frontend/src/api/ChatbotApi.js
@@ -9,27 +9,10 @@ const getConfig = () => {
   };
 };
 
-// Define rate limiting parameters
-const requestsPerMinute = 1; // Number of requests allowed per minute
-const interval = 60000 / requestsPerMinute; // Interval in milliseconds
-
-// Create a queue to manage the requests
-const requestQueue = [];
-
-// Function to make a request and add it to the queue
-const makeRequest = async (url, data) => {
-  if (requestQueue.length >= requestsPerMinute) {
-    // If the queue is full, wait for the next interval
-    await new Promise((resolve) => setTimeout(resolve, interval));
-  }
-  const response = await axiosChatbot.post(url, data, getConfig());
-  return response.data;
-};
-
 export const getResponse = async (message) => {
   try {
     const data = { question: message };
-    return await makeRequest('/get-response', data);
+    return await axiosChatbot.post('/get-response', data, getConfig());
   } catch (err) {
     if (err.code === 'ERR_NETWORK') {
       throw Object.assign(new Error(err.code), {
