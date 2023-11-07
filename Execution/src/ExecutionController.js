5b613fa2c54492cde70296686a0e0722cb9a2f0d
JrmCkh <Hhelloogkart@gmail.com>
2023-11-07 16:00:06 +0800

    Fix bug where no 2 user can exec the same language at the same time

--- Execution/src/ExecutionController.js
+++ Execution/src/ExecutionController.js
@@ -1,10 +1,14 @@
 const fs = require('fs');
 const { spawn } = require('child_process');
+const treeKill = require('tree-kill');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 const {
   MAX_EXECUTION_TIME,
   TIMEOUT_ERROR,
-  SIGNAL_INTERRUPT,
+  SIGNAL_TERMINATE,
   SUCCESS_CODE,
+  SIGNAL_KILL,
 } = require('./constants');
 const logger = require('./Log');
 
@@ -17,16 +21,12 @@ const handleEmptyCode = (res) => {
   res.json({ output: 'No code provided.' });
 };
 
-const executeScript = (scriptPath, childProcess) => {
-  return new Promise((resolve, reject) => {
-    const scriptTimeout = setTimeout(() => {
-      logger.error('Script execution timed out. Killing the script...');
-      childProcess.kill(SIGNAL_INTERRUPT); // Send an interrupt signal to the Python process
-      resolve(
-        `${TIMEOUT_ERROR}: Your program has timed out. Please try again.`
-      );
-    }, MAX_EXECUTION_TIME);
+const uniqueId = () => {
+  return uuidv4();
+};
 
+const executeScript = (scriptPath, childProcess, executionDirectory = null) => {
+  return new Promise((resolve, reject) => {
     const output = [];
     let errorOutput = '';
 
@@ -38,15 +38,66 @@ const executeScript = (scriptPath, childProcess) => {
       errorOutput += data.toString();
     });
 
-    childProcess.on('exit', (code) => {
+    childProcess.on('exit', async (code) => {
       clearTimeout(scriptTimeout);
       if (code === SUCCESS_CODE) {
         resolve(output.join(''));
       } else {
         resolve(errorOutput);
       }
-      fs.unlinkSync(scriptPath);
+
+      // Cleanup of temporary files/directories when process exits
+      try {
+        // Checks if the scriptPath is a file within a directory
+        if (executionDirectory) {
+          fs.unlinkSync(path.join(executionDirectory, scriptPath));
+          logger.logSuccess('Temporary script removed.');
+
+          // JAVA: Directory is not removed until after the .class file is executed
+          if (scriptPath.endsWith('.class')) {
+            await fs.promises.rmdir(executionDirectory, { recursive: true });
+            logger.logSuccess('Temporary directory removed.');
+          }
+        } else {
+          fs.unlinkSync(scriptPath);
+          logger.logSuccess('Temporary script removed.');
+        }
+      } catch (error) {
+        logger.error('Error during cleanup:', error);
+      }
     });
+
+    const scriptTimeout = setTimeout(() => {
+      logger.error('Script execution timed out. Killing the script...');
+
+      const childPid = childProcess.pid; // Get the PID of the child process
+
+      // Kills all of the sub-processes of the child process
+      const treeKillPromise = new Promise((resolve, reject) => {
+        treeKill(childPid, SIGNAL_KILL, (err) => {
+          if (err) {
+            logger.error('Error while killing the process tree');
+            reject(err);
+          } else {
+            logger.log('Sub-processes terminated successfully.');
+            resolve();
+          }
+        });
+      });
+
+      // Wait for child's sub-proccesses to be killed before terminating child process
+      treeKillPromise
+        .then(() => {
+          childProcess.kill(SIGNAL_TERMINATE); // Kill the child process (SIGTERM)
+        })
+        .catch((err) => {
+          logger.error('Error during tree-kill');
+        });
+
+      resolve(
+        `${TIMEOUT_ERROR}: Your program has timed out. Please try again.`
+      );
+    }, MAX_EXECUTION_TIME);
   });
 };
 
@@ -54,7 +105,11 @@ const executeScript = (scriptPath, childProcess) => {
 const executePython = async (req, res) => {
   try {
     const pythonCode = req.body.code;
-    const scriptPath = 'temp_script.py';
+
+    // Use timestamp and unique UUID to generate a unique filename
+    const timestamp = Date.now();
+    const id = uniqueId();
+    const scriptPath = `temp_script_${timestamp}_${id}.py`;
 
     // Check if pythonCode is empty
     if (isCodeEmpty(pythonCode)) {
@@ -78,8 +133,12 @@ const executePython = async (req, res) => {
 const executeJava = async (req, res) => {
   try {
     const javaCode = req.body.code;
-    const javaFilename = 'Main.java';
     const javaClassName = 'Main';
+
+    // Generate a unique temporary directory for each Java execution
+    const timestamp = Date.now();
+    const id = uniqueId();
+    const executionDirectory = `Java_${timestamp}_${id}`;
 
     // Check if javaCode is empty
     if (isCodeEmpty(javaCode)) {
@@ -87,21 +146,27 @@ const executeJava = async (req, res) => {
       return;
     }
 
-    // Write the Java source code to a .java file
-    fs.writeFileSync(javaFilename, javaCode);
+    // Create a temporary directory to store the Java source code
+    fs.mkdirSync(executionDirectory);
 
-    // Compile the Java source code
-    const compileProcess = spawn('javac', [javaFilename]);
-    const compileResult = await executeScript(javaFilename, compileProcess);
+    // Write the Java source code to a .java file within the temporary directory
+    const javaFilename = `${javaClassName}.java`;
+    const javaFilePath = `${executionDirectory}/${javaFilename}`;
+    fs.writeFileSync(javaFilePath, javaCode);
+
+    // Compile the Java source code using the Java source file within the execution directory
+    const compileProcess = spawn('javac', [javaFilename], { cwd: executionDirectory });
+    const compileResult = await executeScript(javaFilename, compileProcess, executionDirectory);
 
     if (compileResult.includes('error')) {
       logger.error('Compilation failed.');
       res.json({ output: compileResult });
+      fs.rmdirSync(executionDirectory, { recursive: true });
     } else {
       logger.logSuccess('Program compiled successfully.');
 
-      const javaProcess = spawn('java', [javaClassName]);
-      const result = await executeScript(javaClassName + '.class', javaProcess);
+      const javaProcess = spawn('java', [javaClassName], { cwd: executionDirectory });
+      const result = await executeScript(javaClassName + '.class', javaProcess, executionDirectory);
 
       logger.logSuccess('Program executed successfully.');
 
@@ -115,7 +180,11 @@ const executeJava = async (req, res) => {
 const executeJs = async (req, res) => {
   try {
     const javascriptCode = req.body.code;
-    const scriptPath = 'temp_script.js';
+
+    // Use timestamp and unique UUID to generate a unique filename
+    const timestamp = Date.now();
+    const id = uniqueId();
+    const scriptPath = `temp_script_${timestamp}_${id}.js`;
 
     // Check if javascriptCode is empty
     if (isCodeEmpty(javascriptCode)) {
