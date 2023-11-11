35865a91ad8a18161586b112a3e14edb3b101866
venuslimm <limjiayivenus@gmail.com>
2023-11-11 16:53:43 +0800

    Final fix for logged in pages frontend

--- frontend/src/pages/SubmissionHistory/SubmissionHistory.js
+++ frontend/src/pages/SubmissionHistory/SubmissionHistory.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import Header from '../../components/Header';
 import { SubmissionList } from '../../components/SubmissionHistory';
-import '../../css/SubmissionHistory.css';
 
 function SubmissionHistory() {
   return (
     <div className='background'>
       <div className='main'>
-        <div className='submission-history'>
-          <Header />
-          <SubmissionList />
-        </div>
+        <Header />
+        <SubmissionList />
       </div>
     </div>
   );
